d99a3f06383697c049fff7b69b50ad0056ae69cb
Waynolt <Waynolt@users.noreply.github.com>
2022-05-15 11:19:48 +0200

    overworld sprite

--- make_zipfile.sh
+++ make_zipfile.sh
@@ -11,7 +11,7 @@ filename="${releases_folder}/Sandbox_E19_v$(cat 'VERSION').zip"
 included_items=(
   'Data/Mods/'
   'Data/Map-05.rxdata'
-  'Graphics/Battlebacks/'
+  'Graphics/'
   'README.md'
   'Pokemon Reborn_ Sandbox Mode - Guide.xlsx'
 )
